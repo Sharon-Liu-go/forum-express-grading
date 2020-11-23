d81a14865efbd0313d60f5332a589e49d31578d5
Sharon-liu-go <innovate72095@gmail.com>
2020-11-23 21:54:06 +0800

    feat:add admin/routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
+
 
 
 module.exports = app => {
@@ -6,8 +8,12 @@ module.exports = app => {
   app.get('/', (req, res) => {
     res.redirect('/restaurants')
   })
-
   app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/admin', (req, res) => {
+    res.redirect('admin/restaurants')
+  })
+  app.get('/admin/restaurants', adminController.getRestaurants)
 
 }
 
