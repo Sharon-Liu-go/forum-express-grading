8b4b8077113ce5ab812aeabcbce9d7a0999ae111
Sharon-liu-go <innovate72095@gmail.com>
2020-11-23 18:04:08 +0800

    feat:initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hanldebars', handlebars())
+app.set('view engine', 'hanldebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
