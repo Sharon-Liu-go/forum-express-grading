dc37da73e0242c7df14744d5eaf6ca083828f0e6
Sharon-liu-go <innovate72095@gmail.com>
2020-11-24 17:48:58 +0800

    add flash-message and modify signup feature

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
+const flash = require('connect-flash')
+const session = require('express-session')
 
 
 app.engine('handlebars', handlebars({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session(
+  { secret: 'secret', resave: false, saveUninitialized: false }
+))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
 
 app.listen(port, () => {
   db.sequelize.sync() //讓models跟資料庫同步
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,12 +7,28 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    return User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(() => res.redirect('/signin'))
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    } else {
+      // confirm unique user
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          req.flash('error_messages', '信箱重複！')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+          })
+            .then(() => {
+              req.flash('success_messages', '成功註冊帳號！')
+              res.redirect('/signin')
+            })
+        }
+      })
+    }
   }
 }
-
 module.exports = userController
