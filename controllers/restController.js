10355f32959141e4e63ac23340d0347063af90ee
Sharon-liu-go <innovate72095@gmail.com>
2020-12-01 21:38:03 +0800

    add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,7 +1,23 @@
+const db = require("../models")
+const restaurant = require("../models/restaurant")
+const { Restaurant, Category } = db
+
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({ nest: true, raw: true, include: [Category] }).then(restaurants => {
+      console.log(restaurants)
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      console.log(data)
+      return res.render('restaurants', { restaurants: data })
+    })
   }
+
+
+
 }
 
 module.exports = restController
